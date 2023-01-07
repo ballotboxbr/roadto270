a6628815ebbd014e7012db5577b15a45550f13bc
fruitcake12345 <48142583+fruitcake12345@users.noreply.github.com>
2023-01-07 11:46:58 +1100

    Update electoral.js

--- electoral.js
+++ electoral.js
@@ -188,12 +188,29 @@ if(beforeCenterText !== afterCenterText) {
 	document.getElementById('totalToWinText').style.left = totalToWinTextLeft + "px"}, animDelay);
 }
 	
-//	$.getJSON('https://www.nbcnews.com/firecracker/api/v2/state-results/2022-elections/georgia-senate-runoff-results', function(data) {document.getElementById('testingBoxText').innerHTML = data.ahead});
+//	$.getJSON('https://www.nbcnews.com/firecracker/api/v2/state-results/2022-elections/georgia-senate-runoff-results', function(data) {document.getElementById('testingBoxText').innerHTML = data.currentTime});
 //	$.get('https://www.nbcnews.com/firecracker/api/v2/state-results/2022-elections/georgia-senate-runoff-results',function(data){document.getElementById('testingBoxText').innerHTML = data.currentTime},'json');
 	
-	fetch('https://www.nbcnews.com/firecracker/api/v2/state-results/2022-elections/georgia-senate-runoff-results')
-	.then(res => res.json()) // the .json() method parses the JSON response into a JS object literal
-  	.then(data => document.getElementById('testingBoxText').innerHTML = data.currentTime);
+	var getJSON = function(url) {
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status == 200) {
+        resolve(xhr.response);
+      } else {
+        reject(status);
+      }
+    };
+    xhr.send();
+  });
+};
+
+getJSON('https://www.nbcnews.com/firecracker/api/v2/state-results/2022-elections/georgia-senate-runoff-results').then(function(data) {
+	document.getElementById('testingBoxText').innerHTML = data.currentTime
+});
 
 var idArray = []; var uncalledList = "";
 $(".state").each(function(){idArray.push(this.id)}); var idArrayInput = idArray.toString();
