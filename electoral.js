24eda863c3fb46f43432966dd03c23488f695dce
fruitcake12345 <48142583+fruitcake12345@users.noreply.github.com>
2023-01-07 16:28:05 +1100

    Update electoral.js

--- electoral.js
+++ electoral.js
@@ -1,6 +1,6 @@
 //SETUP
-var blueCandidateSurname = "JOE BIDEN";
-var redCandidateSurname = "DONALD TRUMP";
+var blueCandidateSurname = "JOE";
+var redCandidateSurname = "DONALD";
 
 var grayStateColor = "rgb(100,105,115)";
 var yellowStateColor = "rgb(247,187,5)";
